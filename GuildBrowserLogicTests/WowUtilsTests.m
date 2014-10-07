b7a8e5351a12a4a0ec41fa8033b21d830c2aa0dc
Vladislav Brylinskiy <vbrylin@DPMACMINI27.local>
2014-10-07 11:17:20 +0300

    Added tests

---
+++ GuildBrowserLogicTests/WowUtilsTests.m
@@ -0,0 +1,49 @@
+//
+//  WowUtilsTests.m
+//  GuildBrowser
+//
+//  Created by Vladislav Brylinskiy on 07/10/14.
+//  Copyright (c) 2014 Charlie Fulton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+#import "WowUtils.h"
+
+@interface WowUtilsTests : XCTestCase
+
+@end
+
+@implementation WowUtilsTests
+
+// 1
+-(void)testCharacterClassNameLookup
+{
+    // 2
+    XCTAssertEqualObjects(@"Warrior",
+                         [WoWUtils classFromCharacterType:1],
+                         @"ClassType should be Warrior");
+    // 3
+    XCTAssertFalse([@"Mage" isEqualToString:[WoWUtils classFromCharacterType:2]]);
+    
+    // 4
+    XCTAssertTrue([@"Paladin" isEqualToString:[WoWUtils classFromCharacterType:2]]);
+    // add the rest as an exercise
+}
+
+- (void)testRaceTypeLookup
+{
+    XCTAssertEqualObjects(@"Human", [WoWUtils raceFromRaceType:1]);
+    XCTAssertEqualObjects(@"Orc", [WoWUtils raceFromRaceType:2]);
+    XCTAssertFalse([@"Night Elf" isEqualToString:[WoWUtils raceFromRaceType:45]]);
+    // add the rest as an exercise
+}
+
+- (void)testQualityLookup
+{
+    XCTAssertEqual(@"Grey", [WoWUtils qualityFromQualityType:1]);
+    XCTAssertFalse([@"Purple" isEqualToString:[WoWUtils qualityFromQualityType:10]]);
+    // add the rest as an exercise
+}
+
+@end
